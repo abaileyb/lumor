abb292ee77811062ac41544e35fbc459be5d7f91
blakealtman123 <blakealtman123@gmail.com>
2015-06-11 21:29:36 +0200

    database item user_id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,20 +14,25 @@ class ItemsController < ApplicationController
 	end
 
 	def create
-		@idea = Idea.find(params[:idea_id])	
-		@item = Item.new
-		@item.name = @idea.name
-		@item.description = @idea.description
-		@item.price = @idea.price
-		@item.photos = @idea.photos
-		
-		@photos = Photo.where(idea_id: @idea.id)
-		@photos.each do |photo|
-			photo.item_id = @item_id
-			photo.save!
+		if params[:idea_id]
+			@idea = Idea.find(params[:idea_id])	
+			@item = Item.new
+			@item.name = @idea.name
+			@item.description = @idea.description
+			@item.price = @idea.price
+			@item.photos = @idea.photos
+			
+			@photos = Photo.where(idea_id: @idea.id)
+			@photos.each do |photo|
+				photo.item_id = @item_id
+				photo.save!
+			@item.user_id = @idea.user_id
+			end
+			@idea.destroy
+		else
+			@item = Item.new(item_params)
+  			@item.user_id = current_user.id
 		end
-
-		@idea.destroy
 
 		@item.save!
 		redirect_to items_path
