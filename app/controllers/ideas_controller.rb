fe86ff2a67e83cbcd5e2c5030db0f1ba73a72205
abaileyb <bailey@baileyberro.com>
2015-06-11 19:59:46 +0200

    fixed author not being able to delete

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -20,7 +20,7 @@ class IdeasController < ApplicationController
 
 	def create
   		@idea = Idea.new(idea_params)
-
+  		@idea.user_id = current_user.id
   		respond_to do |format|
     		if @idea.save
      
