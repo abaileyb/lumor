63fc3be8647534f707df6c5ff16119dbe867064b
abaileyb <bailey@baileyberro.com>
2015-06-11 21:38:12 +0200

    fixed numericality

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,6 @@ class Idea < ActiveRecord::Base
   belongs_to :user
   has_many :photos
   has_many :comments
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name
 end
